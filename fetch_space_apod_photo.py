44b326e52e1bdc5af4a1a7fff78009cd3000e0bd
4moree <46574507+4moree@users.noreply.github.com>
2024-10-16 16:01:39 +0300

    add new function download_image, change name to get_apod_photos

--- fetch_space_apod_photo.py
+++ fetch_space_apod_photo.py
@@ -1,22 +1,16 @@
 import requests
-import os
-from config import nasa_token
-from utils import get_image_extension
+from config import nasa_token, filename_path
+from utils import download_image
 
 
 
-def get_apod_photo(count=1):
+def get_apod_photos(count=1):
     response = requests.get('https://api.nasa.gov/planetary/apod', params={'api_key': nasa_token, 'count': count})
     response.raise_for_status()
-    images = response.json()
+    images_info = response.json()
 
-    for item in images:
-        image_url = item.get('hdurl', item.get('url'))
-        if image_url:
-            image_extension = get_image_extension(image_url)
-            print(f"URL = {image_url}, Расширение = {image_extension}")
-            img_response = requests.get(image_url)
-            img_response.raise_for_status()
-            filename = os.path.join('images', os.path.basename(image_url))
-            with open(filename, 'wb') as f:
-                f.write(img_response.content)
+    for photo in images_info:
+        image_url = photo.get('hdurl', photo.get('url'))
+        if not image_url:
+            continue
+        download_image(image_url, filename_path)
