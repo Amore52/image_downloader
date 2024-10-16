e14ecca02ffaa5259645e23d9e57ac6d2da81726
4moree <46574507+4moree@users.noreply.github.com>
2024-10-16 16:05:07 +0300

    new function download_image

--- utils.py
+++ utils.py
@@ -1,5 +1,20 @@
 import os
+import time
+
+import requests
+
 
 def get_image_extension(image_url): # Получение расширения фотографии
     _, ext = os.path.splitext(image_url)
     return ext
+
+
+def download_image(image_url, filename_path):
+    image_extension = get_image_extension(image_url)
+    timestamp = int(time.time())
+    file_path = os.path.join(filename_path, f'image_{timestamp}{image_extension}')
+
+    response = requests.get(image_url)
+    response.raise_for_status()
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
