74c483be476d893122a47941c9034e65fd8e2643
4moree <46574507+4moree@users.noreply.github.com>
2024-10-16 16:01:07 +0300

    add new function download_image

--- fetch_earth_photos.py
+++ fetch_earth_photos.py
@@ -1,7 +1,8 @@
-import os
 import requests
 from datetime import datetime
 from config import nasa_token, filename_path
+from utils import download_image
+
 
 
 def get_earth_photos(num_photos):
@@ -15,10 +16,4 @@ def get_earth_photos(num_photos):
         date_time = datetime.strptime(photo['date'], '%Y-%m-%d %H:%M:%S')
         formatted_date = date_time.strftime('%Y/%m/%d')
         image_url = f'https://epic.gsfc.nasa.gov/archive/natural/{formatted_date}/png/{photo["image"]}.png'
-
-        image_response = requests.get(image_url)
-        image_response.raise_for_status()
-
-        file_path = os.path.join(filename_path, f'image_{photo["image"]}.png')
-        with open(file_path, 'wb') as file:
-            file.write(image_response.content)
+        download_image(image_url, filename_path)
